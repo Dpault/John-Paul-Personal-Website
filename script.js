4a7143e14dac92a1c68481b540e10b67a7130eb1
Dpault <paulhalcon01@gmail.com>
2022-01-04 15:42:35 +0800

    INTCOMP PERSONAL WEBSITE

---
+++ script.js
@@ -0,0 +1,11 @@
+<script type="text/javascript" src="js/JQuery.js"></script>
+	<script type="text/javascript">
+	$(document).ready(function(){
+		$('.toggle').click(function(){
+			$('.toggle').toggleClass('active')
+			$('nav').toggleClass('active')
+		})
+	})
+
+	</script>
+
